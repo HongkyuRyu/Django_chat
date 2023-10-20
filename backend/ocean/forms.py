a6d5a86b06a028a4c491d5d228bffe4de59dd793
HongkyuRyu <minsubrother@naver.com>
2023-10-20 17:32:51 +0900

    feat: 지도 추가

--- backend/ocean/forms.py
+++ backend/ocean/forms.py
@@ -2,11 +2,14 @@ from django import forms
 
 
 class ObservationForm(forms.Form):
-    obsCode = forms.CharField(label='관측소 코드')
-    date = forms.CharField(label="날짜", help_text='날짜형식:YYYYMMDDHH')
+    x = forms.CharField(label='위도')
+    y = forms.CharField(label='경도')
+
+    # obsCode = forms.CharField(label='관측소 코드')
+    date = forms.CharField(label="날짜", help_text='날짜형식:YYYYMMDD')
 
     def clean_data(self):
         date = self.cleaned_data['date']
-        if not date.isdigit() or len(date) != 10:
-            raise forms.ValidationError('날짜 형식은 YYYYMMDDHH여야 합니다.')
+        if not date.isdigit() or len(date) != 8:
+            raise forms.ValidationError('날짜 형식은 YYYYMMDD여야 합니다.')
         return date
--- backend/ocean/sorting.py
+++ backend/ocean/sorting.py
@@ -1,3 +1,9 @@
 # 가장 가까운 위치에 있는 관측소 반환하는 함수
-def find_near():
-    pass
+
+def get_observation_code(x, y):
+    import pandas as pd
+    df = pd.read_csv(
+        "C:\\Users\\minsu\\Documents\\ocearn\\Django_chat\\backend\\ocean\\data\\관측소.csv", encoding='utf-8')
+    df["거리"] = ((df["위도"] - x)**2 + (df["경도"] - y)**2)**(1/2)
+    df = df.sort_values(by="거리", ascending=True)
+    return df.iloc[0, 0]
